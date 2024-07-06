d4a206a822863f0de197c1d98776ec6ec624619d
Bethuel Khisa <sunguribethuel@gmail.com>
2024-07-06 12:45:18 +0300

    Adds DOM manipulation js code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const element = document.querySelector("main");
+
+document.body.removeChild(element);
+
+const newHeader = document.createElement("h1"); // creating the element or tag
+
+newHeader.textContent = "Bethuel is the champion"; // saying what should be in the tag or element
+newHeader.id = "victory"; //assigning the element an id
+
+document.body.append(newHeader); //specifiying where the element should be in the html
